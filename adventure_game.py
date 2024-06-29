9a355f8a8ca04486dfe663a89ef416976db13834
She2024 <bullybluedog@hotmail.com>
2024-06-29 14:01:41 +1000

    started adding code to build game

--- adventure_game.py
+++ adventure_game.py
@@ -1,3 +1,12 @@
 def game():
 
+    answer= input("Would you like to play? (y/n)")
+        if answer.lower() =='y'
+        print("Welcome to the game")
+        start=True
+        inventory=[]
+    else:
+        print ("Probably for the best...")
     
+
+game()
