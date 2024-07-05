0cbcd98fed7831e5681a7b3f57aa4052b121bda2
She2024 <bullybluedog@hotmail.com>
2024-07-06 09:24:35 +1000

    Restarted game build. Added into and tested funtioning as expected.

---
+++ adventure_game.py
@@ -0,0 +1,21 @@
+import time
+
+
+
+if __name__ == "__main__":
+    while True:
+        print ("Welcome to the Outback Ghost Town.")
+        print("There are questions or challenges to complete to obtain points and you navigate through rooms by choosing to walk, left, right, up or down.")
+        print("We'll need your name so we can record your score. Please enter your name: ")
+        name = input()
+        print(f"Good luck, {name}. Score to beat is: No High score recored.")
+        print("Camping in the outback you come across an abandoned town.")
+        time.sleep (3)
+        print("You decide to explore the old government building. It appears to have served a range of services from including the council chambers and the coroners office.")
+        time.sleep (4)
+        print("Standing at the main reception desk you notice are three buttons labelled A, B, and C. You have no idea what they do.")
+        time.sleep (3)
+        print("It's just too tempting you can't help it you press a button")
+        #playGame()
+        quit()
+        
