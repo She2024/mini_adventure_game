445d5a8dde754bf70d66fcb51a34dd9c7db959a0
She2024 <bullybluedog@hotmail.com>
2024-07-07 10:04:56 +1000

    score tracker not working - overly complex removed and will re-think.Librarian UnboundLocalError: local variable 'score' referenced before assignment- fixed nested score error - by moving riddle to create own function

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # Python Mini Adventure Game
 
-    Player navigates map to get to from the starting point to the finish point and avoid dieing. 
-    They answer questions allong the way to secure points and can choose to answer bouns questions to obtain an extra life, hint, or re-try bonus. 
-    Bonus questions to be located in fixed positions. 
-
+    Player navigates map to get to from the starting point to the finish point and avoids dieing. 
+    They answer questions allong the way to secure points and if lucky may find a hidden object to obtain a key. 
+    
 ## Features 
 
     1. Play the game with questions
-    2. Bouns point tracking
+    2. Bouns and point tracking
     3. High score
--- adventure_game.py
+++ adventure_game.py
@@ -1,50 +1,71 @@
 import time
+import datetime
+import csv
+import json
+import os
+global score
+score = 0
 
-def score():
-  score = 0
+  
 
 global key
 
+key = False
+# Answer question for point
+def riddle():
+  global score
+  print("The riddle is 'I watch you sleep, I haunt you by day. You stare at me and saw nothing, but darkness. What am I?' Enter your answer to escape the room:")
+  riddle = "fear" 
+  riddle = input()
+  if(riddle == "fear"):
+            
+    print("Correct answer. The librarian steps aside and look for an escape from the library.")
+    print("One point awarded")
+    score = score + 1
+    print(f"{name} current score: {score}")
+    library()
+  else:
+    print("Angered by your incorrect answer she killed you.")
+    print(f"{name} current score: {score}")
+    
+  quit()
+
+
 def librarian():
   actions = ["answer","flee"]
-  global key
   print("A librarian appears before you with a riddle, she seems friendly and you have no fear of her. You can answer or flee. What would you like to do?")
   userInput = ""
   while userInput not in actions:
     print("Options: flee/answer")
     userInput = input()
     if userInput == "answer":
-      if key:
-        print("The riddle is 'I watch you sleep, I haunt you by day. You stare at me and saw nothing, but darkness. What am I?' Enter your answer to escape the room:")
-        riddle = input()
-      if userInput == "answer":
-        if(riddle == "fear"):
-            print("Correct answer. The door opens and you escape the library. Grateful to have found the exit you never want to explore abandoned locations again.")
-            print("One point awarded")
-      else:
-        print("Angered by your incorrect answer she killed you.")
-      quit()
+       riddle()
     elif userInput == "flee":
       library()
     else:
       print("Please enter a valid option.")
 
 def library():
-  directions = ["left","right"]
+  directions = ["left","right","up","down"]
   global key
   print("Half the bookshelves are empty and random papers cover the floor. Someone is watching you. Where would you like to go?")
   userInput = ""
   while userInput not in directions:
-    print("Options: left/right/down")
+    print("Options: left/right/up/down")
     userInput = input()
     if userInput == "down":
       print("You open the filing cabinet in front of you and among the dusty trinkets and papers you find a key.")
       print("The tag says it's a skeleton key, it must open all the  locks to the building.")
       key = True
+      library()
     elif userInput == "left":
       reception()
     elif userInput == "right":
       librarian()
+    elif userInput == "up":
+      print("Congrats!!. The door opens and you escape the library. Grateful to have found the exit you never want to explore abandoned locations again.")
+      print(score)
+      quit()  
     else:
       print("Please enter a valid option.")
 
@@ -60,7 +81,7 @@ def coronerOffice():
     if userInput == "left":
       print("Your now in the morgue. A showy figure in a medical gown appears before you, like a scene out of '13 Ghosts'.")
       print("You scream and run face first into an open freezer door knocking yourself out. There is no way you're getting out alive.")
-      print(f"Bad luck, {name}.")
+      print(f"Bad luck, {name}. Current score: {score}")
       print(score)
       quit()
     elif userInput == "up":
@@ -86,7 +107,7 @@ def holdingCells():
         courtRoom()   
       else:
         print("This was not the adventure you had in mind. Face to face with pure evil you feel his spectral hand enter your chest and clamp down on your heart. You die.")
-        print
+        print(score)
       quit()
     elif userInput == "up":
       courtRoom()  
@@ -114,7 +135,6 @@ def courtRoom():
       print("Please enter a valid option.")
 
 
-
 def reception():
   directions = ["left","right","down"]
   print("Standing in the main reception area there's nothing much to see. It looks like someone cleared this place out years ago.")
@@ -135,9 +155,9 @@ def reception():
       print("Please enter a valid option.")
 
 
-
 def playGame() :
     score = 0
+    
     play = ["yes", "no"]
     print("It's so tempting, do you press a button? Enter yes or no:")
     play = input()           
@@ -161,8 +181,6 @@ def playGame() :
         print("You've seen too many horror movies and that table looks like a trap. You scan for an exit out of this room")
     reception()
    
-
-
 
 
 if __name__ == "__main__":
